51d8883bd37009dfa7fcabc59ceb79f405f32f77
home <darkwildhacker@gmail.com>
2025-03-21 16:18:01 +0530

    feat: implement intro activity and update dependencies
    -   Added IntroActivity with a Lottie animation for the splash screen
    -   Created a new `activity_intro
    
    - added new Logo
    
    - Created a default Method for Activity Transition with animation ( fade ) present in INTENTS.java

---
+++ app/src/main/java/com/icst/blockidle/activities/intro/IntroActivity.java
@@ -0,0 +1,54 @@
+/*
+ *  This file is part of Block IDLE.
+ *
+ *  Block IDLE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Block IDLE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with Block IDLE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.icst.blockidle.activities.intro;
+
+import com.icst.blockidle.R;
+import com.icst.blockidle.activities.project_manager.ProjectManagerActivity;
+import com.icst.blockidle.databinding.ActivityIntroBinding;
+import com.icst.blockidle.util.INTENTS;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+	private static final String TAG = "IntroActivity";
+
+	ActivityIntroBinding binding;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		EdgeToEdge.enable(this);
+		binding = ActivityIntroBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+			Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+			return insets;
+		});
+
+		new Handler(getMainLooper()).postDelayed(() -> INTENTS.changeScreen(IntroActivity.this, ProjectManagerActivity.class), 2000);
+	}
+}
---
+++ app/src/main/java/com/icst/blockidle/util/INTENTS.java
@@ -0,0 +1,29 @@
+/*
+ *  This file is part of Block IDLE.
+ *
+ *  Block IDLE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Block IDLE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with Block IDLE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.icst.blockidle.util;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class INTENTS {
+	public static void changeScreen(Activity activity, Class<?> c) {
+		Intent intent = new Intent(activity, c);
+		activity.startActivity(intent);
+		activity.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+	}
+}
